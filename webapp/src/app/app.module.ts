64e3c87b25fb7d799a293713e2ccad78bd8606a2
scottcarrington <scarrington@gmail.com>
2025-02-01 08:46:30 -0700

    serve - remove auth login

--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -7,7 +7,6 @@ import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
 import {AppRoutingModule} from './app-routing.module';
 import {MapComponent} from "@components";
 import {ProjectsComponent} from "./pages/projects/projects.component";
-import {LoginButtonComponent} from "./components/auth0/login-button.component";
 import {AuthModule} from "@auth0/auth0-angular";
 import {environment as env} from "../environments/environment.development"
 import {APIService} from "@services";
@@ -21,7 +20,6 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
     NgbModule,
     AppRoutingModule,
     MapComponent,
-    LoginButtonComponent,
     AuthModule.forRoot({
       ...env.auth0,
     }),
