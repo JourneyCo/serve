adcfa74ed4aa8032defb0ea0e14331164ee21db3
scottcarrington <scarrington@gmail.com>
2025-03-11 16:25:30 -0600

    serve - temporary home page

--- webapp/src/app/routes.ts
+++ webapp/src/app/routes.ts
@@ -2,11 +2,12 @@ import { Route} from '@angular/router';
 import {ProjectsComponent} from "./pages/projects/projects.component";
 import {AdminComponent} from "./pages/admin/admin.component";
 import { authGuardFn } from "@auth0/auth0-angular";
+import {HomeComponent} from "./pages/home/home.component";
 
 export const ROUTES: Route[] = [
   // { path: '', component: MapComponent }, // Default route
   { path: 'projects', component: ProjectsComponent, canActivate: [authGuardFn], },
-  {path: '', pathMatch: 'full', component: AdminComponent},
+  {path: '', pathMatch: 'full', component: HomeComponent},
   // {path: 'profile', loadComponent: () => import('./features/profile/profile.component').then(mod => mod.ProfileComponent), canActivate: [AuthGuard]},
   // {path: 'public', loadComponent: () => import('./features/public/public.component').then(mod => mod.PublicComponent)},
   // {path: 'protected', loadComponent: () => import('./features/protected/protected.component').then(mod => mod.ProtectedComponent), canActivate: [AuthGuard]},
